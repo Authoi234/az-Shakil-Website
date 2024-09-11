ac7cd62daf8a2c60a681923858521319e295f93b
Authoi234 <jawadkhan.authoi@gmail.com>
2024-09-11 18:13:06 +0600

    finding images

---
+++ src/Home/SkillsPage/SkillsCards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SkillsCards = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SkillsCards;
--- src/Home/SkillsPage/SkillsPage.js
+++ src/Home/SkillsPage/SkillsPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import learningImage from "../../assets/Learning-rafiki.png";
+import learningImage from "../../assets/graduation.png";
 
 const SkillsPage = () => {
     const gridItems = [
@@ -11,9 +11,9 @@ const SkillsPage = () => {
     ]
 
     return (
-        <div>
+        <div className='bg-[#173158] py-5'>
             <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1'>
-                
+
             </div>
             <div className='md:w-2/5 w-5/6'>
                 <img className='w-full' src={learningImage} alt="" />
