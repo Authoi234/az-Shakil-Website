7be56570c6942426c00801da3f0de5fdee44d77c
Authoi234 <jawadkhan.authoi@gmail.com>
2024-11-09 15:41:15 +0600

    something happended

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { router } from './Router/router';
+import { useState } from 'react';
 
 function App() {
+
   return (
     <div className="">
+
       <RouterProvider router={router}></RouterProvider>
+
     </div>
   );
 }
